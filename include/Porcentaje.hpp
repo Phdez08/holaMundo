1f02782fab3abd7c6a2ccb781e12cc6a47207de4
Phdez08 <160082978+Phdez08@users.noreply.github.com>
2024-10-15 05:31:42 +0000

    Clase 1

---
+++ include/Porcentaje.hpp
@@ -0,0 +1,19 @@
+#include <stdexcept>
+
+class Porcentaje
+{
+private:
+    int numero;
+public:
+    Porcentaje() {
+        this->numero = 0;
+    }
+
+    ~Porcentaje() {}
+    void Incrementar(){
+
+    }
+    void Disminuir(){
+
+    }
+};
