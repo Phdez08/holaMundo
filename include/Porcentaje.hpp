8c0b1f138d7a5113547af4f4454cc091ed1892d4
Phdez08 <160082978+Phdez08@users.noreply.github.com>
2024-10-16 00:13:53 +0000

    clase2

--- include/Porcentaje.hpp
+++ include/Porcentaje.hpp
@@ -6,14 +6,31 @@ private:
     int numero;
 public:
     Porcentaje() {
-        this->numero = 0;
+        this -> numero = 0;
+        }
+
+    Porcentaje(int numero){
+
+        if (numero > 100 || numero < 0){
+            throw new std::runtime_error("Numero fuera de rango.");
+        }
+        else{
+            this -> numero = numero;
+        }
     }
 
     ~Porcentaje() {}
-    void Incrementar(){
 
+    void Incrementar(int numero){
+        this -> numero += numero;
     }
-    void Disminuir(){
 
+    void Disminuir(int numero){
+        this -> numero -= numero;
     }
-};
+
+    int Imprimir(){
+        return this -> numero;
+    }
+
+    };
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-using namespace std;
+#include <Porcentaje.hpp>
 
-int main(int argc, char const *argv[])
-{
-    cout << "Hola "
-         << "22310256" << endl;
-    return 0;
+int main(){
+    Porcentaje foco;
+    Porcentaje foco2;
+    Porcentaje foco3;
+    Porcentaje foco1;
+
+
+    foco1.Incrementar (10);
+    foco1.Incrementar (10);
+    foco1.Disminuir (4);
+
+    std::cout
+    <<"Foco 1: "
+    <<foco1.Imprimir()
+    <<std::endl;
+  
 }
