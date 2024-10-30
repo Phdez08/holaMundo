685074d6575601c53a2b61e2234748eb1aec7a9b
Phdez08 <160082978+Phdez08@users.noreply.github.com>
2024-10-30 00:17:45 +0000

    clase7

--- makefile
+++ makefile
@@ -2,13 +2,19 @@ bin/programa: src/main.cpp include/*.hpp
 	g++ src/main.cpp -Iinclude -o bin/programa
 
 bin/animacion: src/animacion.cpp include/*.hpp
-	g++ src/animacion.cpp -Include -o bin/animcaion
+	g++ src/animacion.cpp -Include -o bin/animacion -lftxui-screen -lftxui-dom -lftxui-component -std=c++2a
+
+bin/prueba: src/prueba.cpp include/*.hpp
+	g++ src/prueba.cpp -Include -o bin/prueba -lftxui-screen -lftxui-dom -lftxui-component -std=c++2a
 
 ejecutar: bin/programa
 	./bin/programa
 
 ejecutar2: bin/animacion
 	./bin/animacion
+
+ejecutar3: bin/prueba
+	./bin/prueba
 
 Clean: bin/programa
 	rm bin/programa
--- src/prueba.cpp
+++ src/prueba.cpp
@@ -12,28 +12,39 @@ int main(int argc, char const *argv[])
 {
     std::string reset_position;
     int frame = 0;
-    auto can = Canvas(50,50);
-    can.DrawPointLine (0, 0, 10, 10);
+    auto can = Canvas(100,100);
+    can.DrawBlockCircle (15,15,15);
+    can.DrawBlockCircleFilled (10, 8, 1);
+    can.DrawBlockCircleFilled (20, 8, 1);
+    can.DrawBlockEllipseFilled (15, 20, 2, 2);
     auto ColorSupport = vbox({
-                            Terminal::ColorSupport() >= Terminal::Color::Palette16
+                            
 
+                              /*
+                              
+                              Terminal::ColorSupport() >= Terminal::Color::Palette16
                               //if de una sola linea
                                 ? text(" 16 color palette support : Yes")
                               //else de una sola linea
                                 : text(" 16 color palette support : No"),
-
+                              
+                              */
+                              
+                            /*
                             Terminal::ColorSupport() >= Terminal::Color::Palette256
                                 ? text("256 color palette support : Yes")
                                 : text("256 color palette support : No"),
+                            
                             Terminal::ColorSupport() >= Terminal::Color::TrueColor
                                 ? text("       True color support : Yes")
                                 : text("       True color support : No"),
+                            */
                         });
     while (true)
     {
         Screen pantalla = Screen::Create(Dimension::Full(), Dimension::Full());
-        Element personaje = spinner(20, frame);
-        Element lienzo = bgcolor(Color::CadetBlueBis, border(vbox(ColorSupport,personaje,canvas(&can))));
+        Element personaje = spinner(2, frame);
+        Element lienzo = bgcolor(Color::CadetBlueBis, border(vbox(ColorSupport, personaje,canvas(&can))));
         Render(pantalla, lienzo);
         std::cout << reset_position;
         pantalla.Print();
